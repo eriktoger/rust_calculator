9be517b459f3139390843b578ef333bb1cea755b
Erik Töger <erik.toger@gmail.com>
2022-05-09 16:30:51 +0200

    Add subtraction

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 mod operations;
-use operations::addition;
+use operations::{addition, subtraction};
 use std::io;
 
+fn get_user_input() -> String {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    return input;
+}
+
 fn main() {
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).unwrap();
-    let answer = addition(buffer);
-    println!("Answer: {}", answer);
+    let input = get_user_input();
+    if input.contains("+") {
+        println!("{}", addition(input));
+    } else if input.contains("-") {
+        println!("{}", subtraction(input));
+    }
 }
--- src/operations.rs
+++ src/operations.rs
@@ -1,19 +1,36 @@
-pub fn addition(input: String) -> i32 {
-    let split = input.split(|c| c == '+' || c == '\n');
+fn get_values(input: &str, delimiter: char) -> (i32, i32) {
+    let split = input.split(|c| c == delimiter || c == '\n');
     let vec: Vec<&str> = split.collect();
     let first = vec[0].parse::<i32>().unwrap();
     let second = vec[1].parse::<i32>().unwrap();
+    (first, second)
+}
+
+pub fn addition(input: String) -> i32 {
+    let (first, second) = get_values(&input, '+');
     return first + second;
+}
+
+pub fn subtraction(input: String) -> i32 {
+    let (first, second) = get_values(&input, '-');
+    return first - second;
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn add() {
+    fn simple_add() {
         let question = String::from("1+3");
         let calculated_answer = addition(question);
         let correct_answer = 4;
+        assert_eq!(calculated_answer, correct_answer);
+    }
+    #[test]
+    fn simple_subtract() {
+        let question = String::from("3-1");
+        let calculated_answer = subtraction(question);
+        let correct_answer = 2;
         assert_eq!(calculated_answer, correct_answer);
     }
 }
